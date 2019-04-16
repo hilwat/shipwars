692b885cb60d454a7c52e2c59b4e6480b874ec0b
Hilary <watson.hilary@gmail.com>
2019-04-16 08:17:07 +0700

    adding frontpage

---
+++ src/Frontpage/Frontpage.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Frontmenu from "./Frontmenu.js"
+import Hero from "./Hero.js"
+import './Frontpage.css';
+import Footer from "../Gamepage/Footer.js"
+
+
+const Frontpage = () => (
+
+	<div>
+		<div className="board-with-panel">
+			<div className="left-panel">
+			 <Frontmenu></Frontmenu>
+			</div> 
+			<div className="extendedright-panel">
+				<Hero></Hero>
+			</div>
+		</div>
+	<div>
+	<Footer></Footer>
+	</div>
+	</div>
+)
+export default Frontpage;
